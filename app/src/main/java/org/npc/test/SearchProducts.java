308cfc947093edf5602963d6bf7dac51f381999d
rhgraham <rhgraham1130@gmail.com>
2015-11-04 20:33:46 -0600

    Added product list to search product screen
    Switched to relative layout to avoid nested linear layouts.
    updateProductList() will be completed when we build a DTO for the
    transaction

--- app/src/main/java/org/npc/test/SearchProducts.java
+++ app/src/main/java/org/npc/test/SearchProducts.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
 public class SearchProducts extends AppCompatActivity {
 
@@ -17,8 +19,15 @@ public class SearchProducts extends AppCompatActivity {
     public void searchProductButtonOnClick(View view)
     {
         Intent intent = new Intent(this, ProductDetails.class);
-        EditText searchBox = (EditText) this.findViewById(R.id.editText);
+        EditText searchBox = (EditText) this.findViewById(R.id.productSearchBox);
         String query = searchBox.getText().toString();
         this.startActivity(intent.putExtra(this.getResources().getString(R.string.product_id_extras_key), query));
+    }
+
+    public void updateProductList()
+    {
+        ListView list = (ListView) this.findViewById(R.id.products_list_view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, list, myStringArray);
+        list.setAdapter(adapter);
     }
 }

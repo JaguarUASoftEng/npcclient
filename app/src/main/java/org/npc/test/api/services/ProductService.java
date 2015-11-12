f0f6f34779f4930abbb489d2f03ec0ecd7560d72
rhgraham <rhgraham1130@gmail.com>
2015-11-11 21:53:59 -0600

    Using lookupcode URL

--- app/src/main/java/org/npc/test/api/services/ProductService.java
+++ app/src/main/java/org/npc/test/api/services/ProductService.java
@@ -4,6 +4,7 @@ import org.json.JSONObject;
 import org.npc.test.api.enums.ApiLevel;
 import org.npc.test.api.enums.ProductApiMethod;
 import org.npc.test.api.enums.ProductApiRequestStatus;
+import org.npc.test.api.enums.ProductApiValue;
 import org.npc.test.api.interfaces.PathElementInterface;
 import org.npc.test.api.models.Product;
 import org.npc.test.api.models.ProductListing;
@@ -13,9 +14,9 @@ import java.util.List;
 import java.util.UUID;
 
 public class ProductService extends BaseRemoteService {
-    public Product getProduct(UUID productId) {
+    public Product getProduct(String lookupCode) {
         JSONObject rawJsonObject = this.requestSingle(
-            (new PathElementInterface[] { ApiLevel.ONE, ProductApiMethod.PRODUCT }), productId
+            (new PathElementInterface[] { ApiLevel.ONE, ProductApiMethod.PRODUCT, ProductApiValue.LOOKUPCODE}), lookupCode
         );
 
         if (rawJsonObject != null) {

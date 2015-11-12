81641499d2540b546912d62146a54932d9e9f95d
rhgraham <rhgraham1130@gmail.com>
2015-11-11 21:50:20 -0600

    Added additional Product field names

--- app/src/main/java/org/npc/test/api/models/fieldnames/ProductFieldNames.java
+++ app/src/main/java/org/npc/test/api/models/fieldnames/ProductFieldNames.java
@@ -2,7 +2,10 @@ package org.npc.test.api.models.fieldnames;
 
 public class ProductFieldNames {
     public static final String ID = "id";
+    public static final String DESCRIPTION = "description";
     public static final String LOOKUP_CODE = "lookupCode";
+    public static final String IS_ACTIVE = "isActive";
+    public static final String PRICE = "price";
     public static final String COUNT = "count";
     public static final String CREATED_ON = "createdOn";
     public static final String API_REQUEST_MESSAGE = "apiRequestMessage";

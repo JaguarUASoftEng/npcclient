161e1069488f1de8556e96fed2fc066210acc906
rhgraham <rhgraham1130@gmail.com>
2015-11-11 21:51:28 -0600

    Changed base URL
    Note: this only works if you are running an emulator.

--- app/src/main/java/org/npc/test/api/services/BaseRemoteService.java
+++ app/src/main/java/org/npc/test/api/services/BaseRemoteService.java
@@ -167,5 +167,5 @@ public abstract class BaseRemoteService {
     private static final String ACCEPT_REQUEST_PROPERTY = "Accept";
     private static final String JSON_PAYLOAD_TYPE = "application/json";
     private static final String CONTENT_TYPE_REQUEST_PROPERTY = "Content-Type";
-    private static final String BASE_URL = "localhost:8080/com.npc.registerservice/";
+    private static final String BASE_URL = "http://10.0.2.2:8080/com.npc.registerservice/";
 }
